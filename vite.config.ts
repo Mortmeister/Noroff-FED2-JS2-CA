14d9b4dc224b8cbb86719601ab535c1f6d6ed535
Mortmeister <morlillehaug@hotmail.com>
2025-09-30 15:52:51 +0200

    added changes to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,18 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        index: "index.html",
+        feed: "feed.html",
+        login: "login.html",
+        single: "single.html",
+        register: "register.html",
+        update: "update.html",
+        profile: "profile.html",
+        "profile-user": "profile-user.html",
+      },
+    },
+  },
 });
